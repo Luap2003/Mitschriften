5690db22c422ba9161449769995e4889cb1fa22e
Luap2003 <luapglaser2@gmail.com>
2023-05-15 12:53:24 +0200

    Update Integral

--- visualize.py
+++ visualize.py
@@ -2,51 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Definiere die Funktion f(x, y)
-def f(x, y):
-    return x**2 + y**2
+# Define the function z = x * y
+def func(x, y):
+    return x * y
 
-# Erstelle ein Gitter für x und y
-x = np.linspace(0, 2, 100)
-y = np.linspace(0, 2, 100)
-X, Y = np.meshgrid(x, y)
+# Create a meshgrid for x and y
+x = np.linspace(0, 1, 1000)
+y = np.linspace(0, 1, 1000)
+x_grid, y_grid = np.meshgrid(x, y)
 
-# Berechne die Werte von f(x, y) für jeden Punkt im Gitter
-Z = f(X, Y)
+# Check if x^2 + y^2 <= 1 and mask the values that don't satisfy the condition
+mask = x_grid**2 + y_grid**2 <= 1
+z_grid = np.where(mask, func(x_grid, y_grid), np.nan)
 
-# Erstelle die 3D-Darstellung
+# Create a 3D plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(x_grid, y_grid, z_grid)
 
-# Zeichne die durchsichtige Flächenfunktion
-ax.plot_surface(X, Y, Z, alpha=0.5)
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
 
-# Zeichne die Wand entlang der x-Achse, die an jedem Punkt (x, 0) gerade f(x, 0) hoch ist
-x_wall = np.linspace(0, 2, 100)
-y_wall = np.zeros_like(x_wall)
-z_wall = f(x_wall, y_wall)
-
-X_wall, Z_wall = np.meshgrid(x_wall, z_wall)
-Y_wall = np.zeros_like(X_wall)
-
-for i in range(len(x_wall)):
-    ax.plot([x_wall[i], x_wall[i]], [0, 0], [0, z_wall[i]], color='red', lw=1)
-
-# Füge die Beschriftungen hinzu
-label_x = 1
-label_y = 0.2
-label_z = f(label_x, label_y) + 1
-ax.text(label_x, label_y, label_z, 'f(x, y)', fontsize=12)
-
-label_x = 1.1
-label_y = -0.4
-label_z = f(label_x, 0) / 2
-ax.text(label_x, label_y, label_z, 'Inneres Integral', fontsize=12)
-
-# Setze die Achsenbeschriftungen
-ax.set_xlabel('Y')
-ax.set_ylabel('X')
-ax.set_zlabel('Z')
-
-# Zeige die Darstellung
 plt.show()
