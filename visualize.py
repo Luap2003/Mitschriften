76d9a0fc90f92c01dabea4ed98da9c90feb614a1
Luap2003 <luapglaser2@gmail.com>
2023-05-15 16:37:33 +0200

    kleine Erweiterung zum ersten Chapter und Integral

--- visualize.py
+++ visualize.py
@@ -2,26 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Define the function z = x * y
-def func(x, y):
-    return x * y
+# Parameter
+t = np.linspace(0, 8 * np.pi, num=1000)
 
-# Create a meshgrid for x and y
-x = np.linspace(0, 1, 1000)
-y = np.linspace(0, 1, 1000)
-x_grid, y_grid = np.meshgrid(x, y)
+# Spirale-Funktion
+x = np.cos(t)
+y = np.sin(t)
+z = t
 
-# Check if x^2 + y^2 <= 1 and mask the values that don't satisfy the condition
-mask = x_grid**2 + y_grid**2 <= 1
-z_grid = np.where(mask, func(x_grid, y_grid), np.nan)
-
-# Create a 3D plot
+# 3D-Plot erstellen
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x_grid, y_grid, z_grid)
+ax.plot(x, y, z)
 
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_zlabel('Z-axis')
+# Achsenbeschriftung
+ax.set_xlabel('X-Achse')
+ax.set_ylabel('Y-Achse')
+ax.set_zlabel('Z-Achse')
 
+# Titel
+ax.set_title('3D-Aufsteigende Spirale')
+
+# Plot anzeigen
 plt.show()
